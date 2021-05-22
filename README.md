cc834669f1e81e4e87aadc43eed68d9ae5a476bb
sekR4 <sebastian.kraus.jena@gmail.com>
2021-05-22 14:24:54 +0200

    added screenshots

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+# Environment
 conda activate nlp
+
+# Sources
---
+++ test.py
@@ -0,0 +1,16 @@
+import sys
+
+l = [1, 2, 3]
+
+
+def fun(x: list):
+
+    for i in x:
+
+        print(i)
+
+
+# print(sys.executable)
+
+for i in range(len(l)):
+    print(i)
